ff8ad5f0267b514c4928329a3d91144f1242910c
clowndonkey <arvalves@gmail.com>
2026-01-16 19:16:36 +0000

    Update GitHub Pages build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for GitHub Pages
+  base: './',
+  build: {
+    outDir: 'docs',
+  },
 })
